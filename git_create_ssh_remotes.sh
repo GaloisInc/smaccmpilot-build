d92ced2cc28bf7361e97978879006b29a2134afc
Pat Hickey <pat@galois.com>
2013-10-12 16:41:52 -0700

    quick and dirty migration script for adding ssh remotes

---
+++ git_create_ssh_remotes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+git remote add origin-ssh git@github.com:GaloisInc/smaccmpilot-build.git
+git fetch origin-ssh
+
+for SUBMOD in  cbmc-reporter ivory ivory-rtverification \
+	simple-spreadsheet-tools \
+	smaccmpilot-SiK smaccmpilot-stm32f4 tower
+do
+	cd $SUBMOD
+	git remote add origin-ssh git@github.com:GaloisInc/$SUBMOD.git
+	git fetch origin-ssh
+	cd -
+done
+
+cd smaccmpilot-stm32f4/src/gcs/mavlink
+git remote add origin-ssh git@github.com:GaloisInc/mavlink.git
+git fetch origin-ssh
+cd -
+

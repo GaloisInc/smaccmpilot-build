40fb700f9d52282fd5328fb112554f983c1231fd
Lee Pike <leepike@gmail.com>
2013-11-06 14:50:57 -0800

    create_ssh_remotes: add gateway.

--- git_create_ssh_remotes.sh
+++ git_create_ssh_remotes.sh
@@ -3,9 +3,14 @@
 git remote add origin-ssh git@github.com:GaloisInc/smaccmpilot-build.git
 git fetch origin-ssh
 
-for SUBMOD in  cbmc-reporter ivory ivory-rtverification \
-	simple-spreadsheet-tools \
-	smaccmpilot-SiK smaccmpilot-stm32f4 tower
+for SUBMOD in \
+  cbmc-reporter              \
+  ivory ivory-rtverification \
+	simple-spreadsheet-tools   \
+	smaccmpilot-SiK            \
+  smaccmpilot-stm32f4        \
+  smaccmpilot-gcs-gateway    \
+  tower
 do
 	cd $SUBMOD
 	git remote add origin-ssh git@github.com:GaloisInc/$SUBMOD.git
